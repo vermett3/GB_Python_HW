30bae69a6267faed261e11cd66d433640be0d2d5
Sergei <alfredsson@mail.ru>
2020-11-14 20:52:29 +0300

    add file - hw02_1

---
+++ hw02_1.py
@@ -0,0 +1,10 @@
+i = 13
+f = 1.3
+s = "DONT WORRY BE HAPPY"
+l = ['test', '13']
+t = ('test2', '13')
+d = {'team': 'Ottawa', 'league': 'NHL'}
+
+list = [i, f, s, l, t, d]
+for i in list:
+    print(f'{i} is {type(i)}')
